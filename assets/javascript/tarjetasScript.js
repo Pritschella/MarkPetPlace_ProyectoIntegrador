b8f5b80065261dd561ca71129906d6f875e72dcc
Antonio Salcido De Avila <antoniosalcidoav@gmail.com>
2023-10-13 21:27:34 -0600

    tarjetaScript  v3
    Nueva versión del script, casi listo para introducir la fetch api

--- assets/javascript/tarjetasScript.js
+++ assets/javascript/tarjetasScript.js
@@ -1,160 +1,98 @@
-// Carrusel de productos destacados de la seccion de perros
-//Obtengo el contenedor de las tarjetas de cada seccion
-function pintarFilaPerro(contenedorId) {
-    const contenedorTarjetaPerro = document.getElementById(contenedorId);
-    var fila = document.createElement("div"); //Creo un div para cada fila
-    fila.className = "row"; //Le asigno la clase row de bootstrap
-    for (let i = 0; i < 4; i++) { //Creo un for para que se repita 4 veces y se creen 4 tarjetas
-        let tarjeta = document.createElement("div"); //Creo un div para cada tarjeta
-        tarjeta.className = "col-sm-12 col-md-3 col-lg-3"; //Le asigno la clase col-sm-12 col-md-3 col-lg-3 de bootstrap
-        //Le asigno el contenido a cada tarjeta
-        tarjeta.innerHTML = `
-    <div class="product-card">
+function crearTarjeta(contenedor, imgSrc, nombre, categoria, vendedor, precio) {//recibe el contenedor y los datos de la tarjeta
+  const tarjeta = document.createElement("div");//crea un elemento div
+  tarjeta.className = "col-sm-12 col-md-3 col-lg-3";//le asigna una clase al elemento div
+  //innerHTML es una propiedad que define o devuelve el contenido HTML de un elemento
+  tarjeta.innerHTML = `
+      <div class="product-card">
         <div class="product-tumb">
-            <img src="https://www.mayoreototal.mx/cdn/shop/products/000058155m_350x.webp?v=1662653418"alt="">
+          <img src="${imgSrc}" alt="">
         </div>
         <div class="product-details">
-            <h4><a href="">Crocretitas</a></h4>
-            <span class="product-catagory">Alimentacion</span>
-            <p>Kenia</p>
-            <div class="product-bottom-details">
-                <div class="product-price"><small></small>$230.99</div>
-                    <div class="product-links">
-                        <a href=""><i class="fa fa-heart-o" id="icono-card"> </i></a>
-                        <a href=""><i class="fa fa-shopping-bag" id="icono-card"> </i></a>
-                        <a href=""><i class="fa fa-shopping-cart" id="icono-card"></i></a>
-                    </div>
-                </div>
+          <h4><a href="">${nombre}</a></h4>
+          <span class="product-catagory">${categoria}</span>
+          <p>${vendedor}</p>
+          <div class="product-bottom-details">
+            <div class="product-price"><small></small>${precio}</div>
+            <div class="product-links">
+              <a href=""><i class="fa fa-heart-o" id="icono-card"> </i></a>
+              <a href=""><i class="fa fa-shopping-bag" id="icono-card"> </i></a>
+              <a href=""><i class="fa fa-shopping-cart" id="icono-card"></i></a>
             </div>
+          </div>
         </div>
-    </div>
-`
-        fila.appendChild(tarjeta); //Agrego cada tarjeta a la fila
-    } //Cierro el for
-    contenedorTarjetaPerro.appendChild(fila); //Agrego cada fila al contenedor de tarjetas
-} //Cierro la funcion pintarFilaPerro
+      </div>
+    `;
+  contenedor.appendChild(tarjeta);//agrega un nodo al final de la lista de hijos de un nodo padre especificado
+}
 
-// Carrusel de productos destacados de la seccion de gatos
-//Obtengo el contenedor de las tarjetas de cada seccion
-function pintarFilaGato(contenedorId) {
-    const contenedorTarjetaGato = document.getElementById(contenedorId);
-    var fila = document.createElement("div"); //Creo un div para cada fila
-    fila.className = "row"; //Le asigno la clase row de bootstrap
-    for (let i = 0; i < 4; i++) { //Creo un for para que se repita 4 veces y se creen 4 tarjetas
-        let tarjeta = document.createElement("div"); //Creo un div para cada tarjeta
-        tarjeta.className = "col-sm-12 col-md-3 col-lg-3"; //Le asigno la clase col-sm-12 col-md-3 col-lg-3 de bootstrap
-        //Le asigno el contenido a cada tarjeta
-        tarjeta.innerHTML = `
-        <div class="product-card">
-            <div class="product-tumb">
-                <img src="https://m.media-amazon.com/images/I/51OFagHcQFL.jpg"alt="">
-            </div>
-            <div class="product-details">
-                <h4><a href="">Castillo de gatos</a></h4>
-                <span class="product-catagory">Accesorios</span>
-                <p>Kenia</p>
-                <div class="product-bottom-details">
-                    <div class="product-price"><small></small>$230.99</div>
-                        <div class="product-links">
-                            <a href=""><i class="fa fa-heart-o" id="icono-card"> </i></a>
-                            <a href=""><i class="fa fa-shopping-bag" id="icono-card"> </i></a>
-                            <a href=""><i class="fa fa-shopping-cart" id="icono-card"></i></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    `
-        fila.appendChild(tarjeta); //Agrego cada tarjeta a la fila
-    } //Cierro el for
-    contenedorTarjetaGato.appendChild(fila); //Agrego cada fila al contenedor de tarjetas
-} //Cierro la funcion pintarFilaGato
+// Función para pintar una fila de tarjetas
+function pintarFila(contenedorId, datos) {//recibe el id del contenedor y los datos de la tarjeta
+  const contenedor = document.getElementById(contenedorId);//obtiene el elemento por su id
+  const fila = document.createElement("div");//crea un elemento div
+  fila.className = "row";//le asigna una clase al elemento div
+  //crea 4 tarjetas
+  for (let i = 0; i < 4; i++) {
+    crearTarjeta(
+      fila,//contenedor
+      datos.imgSrc,//imagen
+      datos.nombre,//nombre del producto
+      datos.categoria,//categoria
+      datos.vendedor,//vendedor
+      datos.precio//precio
+    );
+  }
 
-// Carrusel de productos destacados de la seccion de peces
-//Obtengo el contenedor de las tarjetas de cada seccion
-function pintarFilaPez(contenedorId) {
-    const contenedorTarjetaPez = document.getElementById(contenedorId);
-    var fila = document.createElement("div"); //Creo un div para cada fila
-    fila.className = "row"; //Le asigno la clase row de bootstrap
-    for (let i = 0; i < 4; i++) { //Creo un for para que se repita 4 veces y se creen 4 tarjetas
-        let tarjeta = document.createElement("div"); //Creo un div para cada tarjeta
-        tarjeta.className = "col-sm-12 col-md-3 col-lg-3"; //Le asigno la clase col-sm-12 col-md-3 col-lg-3 de bootstrap
-        //Le asigno el contenido a cada tarjeta
-        tarjeta.innerHTML = `
-        <div class="product-card">
-            <div class="product-tumb">
-                <img src="https://chedrauimx.vtexassets.com/arquivos/ids/20412915/7500211004376_00.jpg?v=638327521844500000"alt="">
-            </div>
-            <div class="product-details">
-                <h4><a href="">Comida de pez</a></h4>
-                <span class="product-catagory">Alimentación</span>
-                <p>Kenia</p>
-                <div class="product-bottom-details">
-                    <div class="product-price"><small></small>$230.99</div>
-                        <div class="product-links">
-                            <a href=""><i class="fa fa-heart-o" id="icono-card"> </i></a>
-                            <a href=""><i class="fa fa-shopping-bag" id="icono-card"> </i></a>
-                            <a href=""><i class="fa fa-shopping-cart" id="icono-card"></i></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    `
-        fila.appendChild(tarjeta); //Agrego cada tarjeta a la fila
-    } //Cierro el for
-    contenedorTarjetaPez.appendChild(fila); //Agrego cada fila al contenedor de tarjetas
-} //Cierro la funcion pintarFilaGato
+  contenedor.appendChild(fila);//agrega un nodo al final de la lista de hijos de un nodo padre especificado
+}
 
-// Carrusel de productos destacados de la seccion de conejos
-//Obtengo el contenedor de las tarjetas de cada seccion
-function pintarFilaConejo(contenedorId) {
-    const contenedorTarjetaConejo = document.getElementById(contenedorId);
-    var fila = document.createElement("div"); //Creo un div para cada fila
-    fila.className = "row"; //Le asigno la clase row de bootstrap
-    for (let i = 0; i < 4; i++) { //Creo un for para que se repita 4 veces y se creen 4 tarjetas
-        let tarjeta = document.createElement("div"); //Creo un div para cada tarjeta
-        tarjeta.className = "col-sm-12 col-md-3 col-lg-3"; //Le asigno la clase col-sm-12 col-md-3 col-lg-3 de bootstrap
-        //Le asigno el contenido a cada tarjeta
-        tarjeta.innerHTML = `
-        <div class="product-card">
-            <div class="product-tumb">
-                <img src="https://m.media-amazon.com/images/I/817nmi6KcNL._AC_UF1000,1000_QL80_.jpg"alt="">
-            </div>
-            <div class="product-details">
-                <h4><a href="">Cama de conejo</a></h4>
-                <span class="product-catagory">Accesorios</span>
-                <p>Kenia</p>
-                <div class="product-bottom-details">
-                    <div class="product-price"><small></small>$230.99</div>
-                        <div class="product-links">
-                            <a href=""><i class="fa fa-heart-o" id="icono-card"> </i></a>
-                            <a href=""><i class="fa fa-shopping-bag" id="icono-card"> </i></a>
-                            <a href=""><i class="fa fa-shopping-cart" id="icono-card"></i></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    `
-        fila.appendChild(tarjeta); //Agrego cada tarjeta a la fila
-    } //Cierro el for
-    contenedorTarjetaConejo.appendChild(fila); //Agrego cada fila al contenedor de tarjetas
-} //Cierro la funcion pintarFilaGato
+// Datos específicos para cada categoría
+const datosPerro = {//objeto con los datos de la tarjeta de perro
+  imgSrc:
+    "https://www.mayoreototal.mx/cdn/shop/products/000058155m_350x.webp?v=1662653418",
+  nombre: "Crocretitas",
+  categoria: "Alimentacion",
+  vendedor: "Kenia",
+  precio: "$230.99",
+};
 
-//Llamo a la funcion para que se ejecute y se pinten las tarjetas de cada seccion en el html
-pintarFilaPerro("tarjeta-perro-contenedor1");
-pintarFilaPerro("tarjeta-perro-contenedor2");
-pintarFilaPerro("tarjeta-perro-contenedor3");
+const datosGato = {//objeto con los datos de la tarjeta de gato
+  imgSrc: "https://m.media-amazon.com/images/I/51OFagHcQFL.jpg",
+  nombre: "Castillo de gatos",
+  categoria: "Accesorios",
+  vendedor: "Kenia",
+  precio: "$230.99",
+};
 
-pintarFilaGato("tarjeta-gato-contenedor1");
-pintarFilaGato("tarjeta-gato-contenedor2");
-pintarFilaGato("tarjeta-gato-contenedor3");
+const datosPez = {//objeto con los datos de la tarjeta de pez
+  imgSrc:
+    "https://chedrauimx.vtexassets.com/arquivos/ids/20412915/7500211004376_00.jpg?v=638327521844500000",
+  nombre: "Comida de pez",
+  categoria: "Alimentación",
+  vendedor: "Kenia",
+  precio: "$230.99",
+};
 
-pintarFilaPez("tarjeta-pez-contenedor1");
-pintarFilaPez("tarjeta-pez-contenedor2");
-pintarFilaPez("tarjeta-pez-contenedor3");
+const datosConejo = {//objeto con los datos de la tarjeta de conejo
+  imgSrc: "https://m.media-amazon.com/images/I/817nmi6KcNL._AC_UF1000,1000_QL80_.jpg",
+  nombre: "Cama de conejo",
+  categoria: "Accesorios",
+  vendedor: "Kenia",
+  precio: "$230.99",
+};
 
-pintarFilaConejo("tarjeta-conejo-contenedor1");
-pintarFilaConejo("tarjeta-conejo-contenedor2");
-pintarFilaConejo("tarjeta-conejo-contenedor3");
+// Pintar las filas de tarjetas con los datos específicos de cada categoría
+pintarFila("tarjeta-perro-contenedor1", datosPerro);//llama a la funcion pintarFila y le pasa el id del contenedor y los datos de la tarjeta
+pintarFila("tarjeta-perro-contenedor2", datosPerro);
+pintarFila("tarjeta-perro-contenedor3", datosPerro);
+
+pintarFila("tarjeta-gato-contenedor1", datosGato);
+pintarFila("tarjeta-gato-contenedor2", datosGato);
+pintarFila("tarjeta-gato-contenedor3", datosGato);
+
+pintarFila("tarjeta-pez-contenedor1", datosPez);
+pintarFila("tarjeta-pez-contenedor2", datosPez);
+pintarFila("tarjeta-pez-contenedor3", datosPez);
+
+pintarFila("tarjeta-conejo-contenedor1", datosConejo);
+pintarFila("tarjeta-conejo-contenedor2", datosConejo);
+pintarFila("tarjeta-conejo-contenedor3", datosConejo);
