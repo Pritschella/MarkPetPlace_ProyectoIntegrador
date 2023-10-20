16b3bb80d95362050fcc24f1e020ab6513b8ab24
UrielRbls <66899453+UrielRbls@users.noreply.github.com>
2023-10-20 05:24:16 -0600

    Seccion mascota y local storage login&registro
    Se agrega login y registro a traves de localstorage, se sube pagina seccion mascota

--- assets/javascript/categoriaMascota.js
+++ assets/javascript/categoriaMascota.js
@@ -1,6 +1,19 @@
 'use strict';
 
-CloseBtn.addEventListener('click', modalCloseFunc);
+// modal variables
+const modal = document.querySelector('[data-modal]');
+const modalCloseBtn = document.querySelector('[data-modal-close]');
+const modalCloseOverlay = document.querySelector('[data-modal-overlay]');
+
+// modal function
+const modalCloseFunc = function () { modal.classList.add('closed') }
+
+// modal eventListener
+modalCloseOverlay.addEventListener('click', modalCloseFunc);
+modalCloseBtn.addEventListener('click', modalCloseFunc);
+
+
+
 
 
 // notification toast variables
