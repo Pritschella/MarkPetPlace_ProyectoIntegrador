b0dcf264d8a48e8e8132c62b152eb8efd79cbce3
Kenia Montalvo <kenlimonra@gmail.com>
2023-10-26 17:19:54 -0600

    se agregó mecanismo por completar de la barra de búsqueda

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -28,3 +28,21 @@ fetch(footerURL)
     .catch(error => {
         console.log("Error al cargar el footer", error);
     })
+
+
+/*Buscador*/
+document.addEventListener("DOMContentLoaded", function () {
+    const searchForm = document.getElementById("search-form");
+    const searchInput = document.getElementById("search-input");
+
+    searchForm.addEventListener("submit", function (event) {
+        event.preventDefault(); // Evitar que el formulario se envíe normalmente
+
+        // Obtener el término de búsqueda ingresado por el usuario
+        const searchTerm = searchInput.value;
+
+        // Puedes redirigir al usuario a una página de resultados de búsqueda o realizar una acción personalizada aquí
+        // Por ejemplo, redirigir al usuario a una página de resultados de búsqueda
+        window.location.href = "/busqueda?query=" + encodeURIComponent(searchTerm);
+    });
+});
